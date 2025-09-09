1d03093762ee1263cd2725414fd07216158f97a2
Jaydeep Raval <rjaydeep@joulepoint.com>
2025-09-09 11:37:43 +0530

    second commit

--- app/src/main/java/com/example/mvvmdemo/bindingutils/ActivityBindingAdapter.kt
+++ app/src/main/java/com/example/mvvmdemo/bindingutils/ActivityBindingAdapter.kt
@@ -185,6 +185,7 @@ fun sum(a: Double, b: Double): Double {
 fun calculator(a: Double, b: Double, gn: (Double, Double) -> Double) {
     val result = gn(a, b)
     println("Higher Order Fun: $result")
+    println("Complete works")
 }
 
 
